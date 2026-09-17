3b1a30d67495c445f30b9446f19ce2c9202c83bc
Ryan Mitchell <ryan@mitchell.plus>
2026-09-17 17:05:21 -0400

    Update the fzf plugin.

--- plugins/fzf/fzf.plugin.sh
+++ plugins/fzf/fzf.plugin.sh
@@ -1,6 +1,7 @@
 THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if command -v fzf &>/dev/null; then
+  [[ -f /usr/share/fzf/shell/key-bindings.bash ]] && source /usr/share/fzf/shell/key-bindings.bash # keybindings in Fedora
   [[ -f ~/.fzf.bash ]] && source ~/.fzf.bash
 else
   echo "Please install 'fzf' or disable the Bash.env plugin: https://github.com/junegunn/fzf#installation"
